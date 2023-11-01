19dac804256139d6a7aa9c795e26cdb1a73f476d
anumkonda <hanumanjosh@gmail.com>
2023-11-01 10:59:57 +0530

    changes

--- Sep_20/lib/dialog.js
+++
@@ -1,9 +0,0 @@
-var closeDialogBox = () => {
-    document.querySelector(".dialogContainer").style.display = 'none';
-    document.querySelector(".maskBlock").style.display = 'none';
-}
-
-var openDialogBox = () => {
-    document.querySelector(".maskBlock").style.display = 'block';
-    document.querySelector(".dialogContainer").style.display = 'block';
-}
